7758285fa3616e8f28005b1772dd3e85c6978f1f
unknown <krzys.repsch@gmail.com>
2022-11-12 16:11:51 +0100

    Refactor(frontend): Cleanup

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,10 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+/**
+ * External dependencies
+ */
+import type { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }: AppProps) => (
+	<Component {...pageProps} />
+);
 
 export default App;
