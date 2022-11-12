625783a83f981c860a53dc79e20ea6569a9cc384
unknown <krzys.repsch@gmail.com>
2022-11-12 17:30:06 +0100

    Feat(frontend): Add theme.ts file

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,19 @@
  * External dependencies
  */
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+/**
+ * External dependencies
+ */
+import { theme } from '../src/styles/theme';
 import GlobalStyle from '../src/styles/globalStyles';
 
 const App = ({ Component, pageProps }: AppProps) => (
-	<>
+	<ThemeProvider theme={theme}>
 		<GlobalStyle />
 		<Component {...pageProps} />
-	</>
+	</ThemeProvider>
 );
 
 export default App;
--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -4,10 +4,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+
 	*,*::before, *::after {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+		font-weight: 400;
+		font-family: 'Poppins', sans-serif;
+		color: ${({ theme }) => theme.colors.white};
+	}
+
+	body {
+		background: ${({ theme }) => theme.colors.black};
 	}
 `;
 
