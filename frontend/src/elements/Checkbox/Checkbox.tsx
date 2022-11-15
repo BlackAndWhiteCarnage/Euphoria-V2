211aed71fccc6b5f8c99061b056901fbc955625d
unknown <krzys.repsch@gmail.com>
2022-11-15 18:07:39 +0100

    Feat(frontend): Add Checkbox component

---
+++ frontend/src/elements/Checkbox/Checkbox.tsx
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { FC, InputHTMLAttributes } from 'react';
+
+/**
+ * External dependencies
+ */
+import classes from './Checkbox.module.scss';
+
+type CheckboxProps = {
+	checked?: boolean;
+	id: string;
+	onChange?: (value: boolean) => void;
+} & Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>;
+
+/**
+ * Checkbox component
+ */
+const Checkbox: FC<CheckboxProps> = ({
+	checked = false,
+	id,
+	onChange,
+	...props
+}) => (
+	<label htmlFor={id} className={classes.checkbox}>
+		<input
+			{...props}
+			type="checkbox"
+			id={id}
+			className={classes.input}
+			checked={checked}
+			onChange={() => onChange && onChange(!checked)}
+		/>
+		<span className={classes.checkboxMark} />
+	</label>
+);
+
+export default Checkbox;
