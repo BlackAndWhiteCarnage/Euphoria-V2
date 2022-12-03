fac0f5e36901a1f1bbcba92b3e72ba578e35fc8d
unknown <krzys.repsch@gmail.com>
2022-12-03 14:55:53 +0100

    Feat(frontend): Add WithFormLayout

--- frontend/src/layouts/WithFormLayout/WithFormLayout.tsx
+++ frontend/src/layouts/WithFormLayout/WithFormLayout.tsx
@@ -9,18 +9,19 @@ import classnames from 'classnames';
  * Internal dependencies
  */
 import { Box, Button } from 'elements';
+import { useUser } from '@auth0/nextjs-auth0';
+import { email } from 'config/patterns';
 import classes from './WithFormLayout.module.scss';
 
 const WithFormLayout: FC<PropsWithChildren> = ({ children }) => {
+	const { user, isLoading } = useUser();
+
 	const {
 		register,
 		handleSubmit,
-		watch,
 		formState: { errors },
 	} = useForm();
 	const onSubmit = (data) => console.log(data);
-
-	console.log(errors);
 
 	return (
 		<>
@@ -30,37 +31,41 @@ const WithFormLayout: FC<PropsWithChildren> = ({ children }) => {
 					Nie znalazłeś tego czego szukałeś lub masz pytania?{' '}
 					<span className={classes.decorator}>Pisz śmiało!</span>
 				</h3>
-				<Box>
-					<form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-						<label htmlFor="email" className={classes.label}>
-							Email
-						</label>
-						<input
-							id="email"
-							type="text"
-							className={classnames(classes.input, {
-								[classes['has-error']]: errors.email,
-							})}
-							{...register('email', {
-								required: true,
-								pattern:
-									/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-							})}
-						/>
-						<label htmlFor="message" className={classes.label}>
-							Wiadomość
-						</label>
-						<textarea
-							id="message"
-							className={classnames(classes.textarea, {
-								[classes['has-error']]: errors.message,
-							})}
-							maxLength={400}
-							{...register('message', { required: true })}
-						/>
-						<Button size="large">Wyślij</Button>
-					</form>
-				</Box>
+				{!isLoading && (
+					<Box>
+						<form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+							<label htmlFor="email" className={classes.label}>
+								Email
+							</label>
+							<input
+								id="email"
+								type="text"
+								// @ts-ignore
+								value={user && user.email}
+								className={classnames(classes.input, {
+									[classes['has-error']]: errors.email,
+									[classes['is-disabled']]: user?.email,
+								})}
+								{...register('email', {
+									required: true,
+									pattern: email,
+								})}
+							/>
+							<label htmlFor="message" className={classes.label}>
+								Wiadomość
+							</label>
+							<textarea
+								id="message"
+								className={classnames(classes.textarea, {
+									[classes['has-error']]: errors.message,
+								})}
+								maxLength={400}
+								{...register('message', { required: true })}
+							/>
+							<Button size="large">Wyślij</Button>
+						</form>
+					</Box>
+				)}
 			</div>
 		</>
 	);
