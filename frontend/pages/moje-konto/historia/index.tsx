f4bbd741623b099bfd09979dd4e0b8f6d4ae817f
unknown <krzys.repsch@gmail.com>
2022-12-09 23:07:14 +0100

    test 2

--- frontend/pages/moje-konto/historia/index.tsx
+++ frontend/pages/moje-konto/historia/index.tsx
@@ -14,45 +14,45 @@ const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET}`, {
 	apiVersion: '2022-11-15',
 });
 
-export const getServerSideProps = withPageAuthRequired({
-	async getServerSideProps(ctx) {
-		const session = getSession(ctx.req, ctx.res);
+// export const getServerSideProps = withPageAuthRequired({
+// 	async getServerSideProps(ctx) {
+// 		const session = getSession(ctx.req, ctx.res);
 
-		const stripeId =
-			session?.user[`${process.env.AUTH0_BASE_URL}/stripe_customer_id`];
+// 		const stripeId =
+// 			session?.user[`${process.env.AUTH0_BASE_URL}/stripe_customer_id`];
 
-		const paymentIntents = await stripe.paymentIntents.list({
-			customer: stripeId,
-		});
+// 		const paymentIntents = await stripe.paymentIntents.list({
+// 			customer: stripeId,
+// 		});
 
-		const sessions = await stripe.checkout.sessions.list({
-			customer: stripeId,
-			limit: 50,
-		});
+// 		const sessions = await stripe.checkout.sessions.list({
+// 			customer: stripeId,
+// 			limit: 50,
+// 		});
 
-		const sessionsIds: Array<string> = [];
+// 		const sessionsIds: Array<string> = [];
 
-		sessions.data.forEach(({ id }) => sessionsIds.push(id));
+// 		sessions.data.forEach(({ id }) => sessionsIds.push(id));
 
-		const lineItems = await Promise.all(
-			sessionsIds.map((id) =>
-				stripe.checkout.sessions.listLineItems(id, { limit: 50 })
-			)
-		);
+// 		const lineItems = await Promise.all(
+// 			sessionsIds.map((id) =>
+// 				stripe.checkout.sessions.listLineItems(id, { limit: 50 })
+// 			)
+// 		);
 
-		return {
-			props: { orders: { paymentIntents, lineItems } },
-		};
-	},
-});
+// 		return {
+// 			props: { orders: { paymentIntents, lineItems } },
+// 		};
+// 	},
+// });
 
 const OrdersHistory = (orders: any) => (
 	<>
 		<Head>
 			<title>EUPHORIA | Historia Zamówień</title>
 		</Head>
-		{console.log(orders)}
-		<OrdersHistoryLayout data={orders} />
+		{/* <OrdersHistoryLayout data={orders} /> */}
+		<div>dupa</div>
 	</>
 );
 
