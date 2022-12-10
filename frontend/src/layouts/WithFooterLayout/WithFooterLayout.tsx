ee3ab4e99b6a22dc895c19cd54c8523fd10d4d07
unknown <krzys.repsch@gmail.com>
2022-12-10 19:49:22 +0100

    sdaasd

--- frontend/src/layouts/WithFooterLayout/WithFooterLayout.tsx
+++ frontend/src/layouts/WithFooterLayout/WithFooterLayout.tsx
@@ -14,19 +14,21 @@ import classes from './WithFooterLayout.module.scss';
 const WithFooterLayout: FC<PropsWithChildren> = ({ children }) => (
 	<>
 		{children}
-		<footer className={classes.footer}>
-			<div className={classes.content}>
-				<div className={classes.paymentMethods}>
-					<b>Metody płatności:</b>
-					<Przelewy24 className={classes.icon} />
-					<Blik className={classes.icon} />
+		<div className={classes.wrapper}>
+			<footer className={classes.footer}>
+				<div className={classes.content}>
+					<div className={classes.paymentMethods}>
+						<b>Metody płatności:</b>
+						<Przelewy24 className={classes.icon} />
+						<Blik className={classes.icon} />
+					</div>
 				</div>
-			</div>
-			<div className={classes.buttons}>
-				<Button href="/polityka-prywatnosci">Polityka prywatności</Button>
-				<Button href="/regulamin">Regulamin</Button>
-			</div>
-		</footer>
+				<div className={classes.buttons}>
+					<Button href="/polityka-prywatnosci">Polityka prywatności</Button>
+					<Button href="/regulamin">Regulamin</Button>
+				</div>
+			</footer>
+		</div>
 	</>
 );
 
